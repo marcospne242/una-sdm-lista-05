223a7ab630729cdb0dfa03c862f2298f34e52d1b
marcospne242 <marcospne242@gmail.com>
2026-03-15 11:00:12 -0300

    Subindo arquivos da att

---
+++ Lista05/Models/Usuario.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lista05.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ Lista05/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Lista05.Models;
+
+
+Todo todoEx = new Todo
+{
+    UserId = 1,
+    Id = 1,
+    Title = "delectus aut autem",
+    Completed = false
+};
+
+Console.WriteLine($"To-do: {todoEx.Title} | Completo: {todoEx.Completed}");
+
+Post postEx = new Post
+{
+    UserId = 1,
+    Id = 101,
+    Title = "Introdução ao C#",
+    Body = "Este post explica os conceitos básicos de C# e .NET."
+};
+
+Console.WriteLine($"Post: {postEx.Title} | Conteúdo: {postEx.Body}");
+
+Usuario usuarioEx = new Usuario
+{
+    Id = 1,
+    Name = "Maria Silva",
+    Username = "maria.s",
+    Email = "maria.silva@email.com",
+    Phone = "31-99999-8888"
+};
+
+Console.WriteLine($"Usuário: {usuarioEx.Name} | Email: {usuarioEx.Email}");
